b762bcb068b4b1032f6337742a95173ed959e9fd
Aldona <aldonapawlak2@gmail.com>
2020-08-30 19:51:42 +0200

    Refactor - add atomic methods

--- src/main/java/guestbook/handlers/Guestbook.java
+++ src/main/java/guestbook/handlers/Guestbook.java
@@ -17,30 +17,46 @@ public class Guestbook implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
 
         if (exchange.getRequestMethod().equals("POST")) {
-            InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
-            BufferedReader br = new BufferedReader(isr);
-            String formData = br.readLine();
-
+            String formData = transformBodyToString(exchange);
             Map<String, String> inputs = parseFormData(formData);
-
-            String entryName = inputs.get("name");
-            String entryMessage = inputs.get("message");
-            GuestbookEntry guestbookEntry = new GuestbookEntry(entryName, entryMessage, new Date());
-
-            guestbookEntries.add(guestbookEntry);
+            addGuestbookEntry(inputs);
         }
 
+        String response = createTemplate();
+
+        sendResponse(exchange, response);
+    }
+
+    private void sendResponse(HttpExchange exchange, String response) throws IOException {
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+
+    private String createTemplate() {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/guestbook.twig");
         JtwigModel model = JtwigModel.newModel();
 
         model.with("entries", guestbookEntries);
 
         String response = template.render(model);
+        return response;
+    }
 
-        exchange.sendResponseHeaders(200, response.length());
-        OutputStream os = exchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+    private void addGuestbookEntry(Map<String, String> inputs) {
+        String entryName = inputs.get("name");
+        String entryMessage = inputs.get("message");
+        GuestbookEntry guestbookEntry = new GuestbookEntry(entryName, entryMessage, new Date());
+
+        guestbookEntries.add(guestbookEntry);
+    }
+
+    private String transformBodyToString(HttpExchange exchange) throws IOException {
+        InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+        String formData = br.readLine();
+        return formData;
     }
 
     private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
